7ffdbdf6ed37790bfe3bb82128943980f186c192
Duder <thedudeabides1981@hotmail.com>
2022-08-18 23:58:18 -0700

    added pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+/* PSEUDOCODE:
+1. When city name is entered and submitted with "search" button, city's current weather populates "current-weather" div:
+- city name, current date, temp icon
+- current temp
+- wind
+- current humidity
+- UV index 
+2. City information is saved to LocalStorage
+3. Then daily forecasts for 5 days populate 5-day row:
+- date
+- temp icon
+- temp
+- wind
+- humidity
+5. Searched cities in LocalStorage populate buttons in "search-history" div.
+6. Click buttons in "search-history" to display weather conditions for those cities
+ */
